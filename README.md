387b21f6837668b298e551bb7963e33b305112d6
Andreas Schröpfer <andreas@apxp.info>
2020-02-14 16:11:29 +0100

    documentation

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # frontmatter
 Parses an io.Reader and extracts the frontmatter 
+
+## Syntax
+
+When you need to store some additional information inside a text you can use a frontmatter logic for doing this. That section ist defines by a delimiter, which should be not used inside the text. Inside that section you can define simple variables with values. 
+
+    +++
+    key1 = value1
+    key 2 = another seconde value
+    +++
+    # Some header here
+
+    some more text here
+
+So the delimiter here is `+++` that part will be extracted inside a map.
+
+```go
+doc := `+++
+key1 = first key
+key2 = 2ndKey 
++++
+# My Document
+...
+`
+b := &bytes.Buffer{}
+fm, _ := Parse("+++", strings.NewReader(doc), b)
+fmt.Printf("%#v\n", fm)
+fmt.Println(b)
+// Output: map[string]string{"key1":"first key", "key2":"2ndKey"}
+// # My Document
+// ...
+```
--- parse.go
+++ parse.go
@@ -1,3 +1,12 @@
+// Package frontmatter provides a simple way to extract additional informations out of a text.
+//
+// It is easy to use it. First you need a delimiter, which defines
+// the beginning and the end of the frontmatter section.
+// The syntax for adding a value:
+//
+//   key = some value
+//
+// You can use every string as key and every string as value.
 package frontmatter
 
 import (
@@ -8,8 +17,9 @@ import (
 )
 
 // Parse extrakts frontmatter information out of a io.Reader. The function
-// returns the frontmatter as a simple map[string]string. The body is returned
-// as io.Reader.
+// returns the frontmatter as a simple map[string]string. The body part
+// of the text is written into an io.Writer. That makes that
+// function useful, for using it inside a handler.
 func Parse(delim string, r io.Reader, w io.Writer) (frontmatter map[string]string, err error) {
 	frontmatter = make(map[string]string)
 	scanner := bufio.NewScanner(r)
